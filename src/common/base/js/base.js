5a7290669eb822d62ecfadc4cf6235ea37c853af
堵杰 <760813193@qq.com>
2015-07-08 13:07:06 +0800

    commit

--- src/common/base/js/base.js
+++ src/common/base/js/base.js
@@ -3,7 +3,9 @@ var baseScriptEl = scripts[scripts.length - 1];
 var baseUrl = baseScriptEl.getAttribute("src").match(/^(\.\.\/)*/g) || "../../";
 // 保存app应用的前缀路径（发布应用包的时候应用名称会改变）
 window.APP_PATH = location.pathname.match(/\bapp\/.+\//);
+
 seajs.config({
+	debug:true,
 	base:baseUrl,
 	paths:{
 		"base":"common/base",
@@ -11,16 +13,20 @@ seajs.config({
 		"components":"common/components"
 	},
 	alias:{
+		"seajs-css":"lib/seajs/seajs-css",
+		"seajs-text":"lib/seajs/seajs-text",
+		"seajs-log":"lib/seajs/seajs-log",
 		"jquery":"lib/jquery/jquery.min",
 		"ejs":"lib/ejs/ejs.min",
 		"hammer":"lib/hammer/hammer.min",
-		"seajs-css":"lib/seajs/seajs-css",
-		"seajs-text":"lib/seajs/seajs-text",
 		"d3":"lib/d3/d3.min",
 		"login":"base/js/login"
 	}
 });
-seajs.use(["jquery","ejs","seajs-css","seajs-text"]);
+
+seajs.use(["jquery","seajs-css","seajs-text","seajs-log","ejs"]);
+
+
 
 
 
