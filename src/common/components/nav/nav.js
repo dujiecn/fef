4e36dca89f20db064a5f0a53a80fb71e70d80d7b
堵杰 <760813193@qq.com>
2015-07-12 11:38:03 +0800

    commit

--- src/common/components/nav/nav.js
+++ src/common/components/nav/nav.js
@@ -1,8 +1,23 @@
 define(function(require) {
 	require('./nav.css');
 
-	var navs = $('.navbar-brand');
+	// 初始化页面导航配置标签
+	$('.navbar').each(function(i) {
+		var that = $(this);
+		var container = $('<nav class="navbar-header">');
+		var navs = that.data('navs').split(",");
+		var active = that.data('active');
+		$.each(navs,function(j,d) {
+			container.append(
+				'<a class="navbar-brand'+(d == active ? ' active' : '')+'" href="javascript:void(0);">'+d+'</a>'
+			);
+		});
+		that.append(container);
+	});
 
+
+
+	var navs = $('.navbar-brand');
 	var Hammer = require('hammer');
 
 	// 绑定导航事件，这里的事件不完全，如果有需要，后面自己切换页面的时候补充ajax请求
